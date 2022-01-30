09a5e78b1295b5a73528f18922a8dafda7b118d3
RobertHJones <roberthuwjones@outlook.com>
2022-01-30 21:28:29 +0000

    sorted

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,6 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
-import css from "./Index.module.css";
+import css from "./Login.module.css";
 
 const { loginPage, header, button } = css;
 
