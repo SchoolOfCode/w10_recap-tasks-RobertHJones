e47751a0e092327b7dcac0ff9aad320def03322a
RobertHJones <roberthuwjones@outlook.com>
2022-01-30 17:18:50 +0000

    login page details

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,5 +1,8 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
+import css from "./Index.module.css";
+
+const { loginPage, header, button } = css;
 
 const Login = () => {
   const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
@@ -12,9 +15,11 @@ const Login = () => {
           Log Out
         </button>
       ) : (
-        <div>
-          <h1>Please log in to be able to view this page</h1>
-          <button onClick={() => loginWithRedirect()}>Log in</button>
+        <div className={loginPage}>
+          <h1 className={header}>Please log in to be able to view this page</h1>
+          <button className={button} onClick={() => loginWithRedirect()}>
+            Log in
+          </button>
         </div>
       )}{" "}
     </div>
